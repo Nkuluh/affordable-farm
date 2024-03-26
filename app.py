0d55071d4404739e25509f7774cbca45c6ba2fd0
Nkuluh <127388356+Nkuluh@users.noreply.github.com>
2024-03-26 01:20:59 +0000

    added data and api route

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, jsonify
 
 app = Flask(__name__)
 
+Fruits = [
+  {
+    'id': 1,
+    'name': 'Apple',
+    'price': 'R.20.00',
+  },
+  {
+    'id': 2,
+    'name': 'Banana',
+    'price': 'R.35.00',
+  },
+  {
+    'id': 3,
+    'name': 'Mango',
+    'price': 'R.50.00',
+  },
+  {
+    'id': 4,
+    'name': 'Pear',
+    'price': 'R.25.50',
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', fruits=Fruits)
+
+@app.route("/api/fruits")
+def list_fruits():
+  return jsonify(Fruits)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
